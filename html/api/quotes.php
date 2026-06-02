6f56109d3037d7261cf12eeec345912acf44a5d8
Phil Hajjar <philhajjar@gmail.com>
2026-06-01 22:07:12 -0400

    Added extra files for quotes support

---
+++ html/api/quotes.php
@@ -0,0 +1,100 @@
+<?php
+
+class Quotes extends OutputJSON
+{
+	private $helpMessage = "";
+	private $conn = "";
+	private $method = "";
+	private $maxQuoteLength = 0;
+
+	function CreateTableIfNeeded()
+	{
+		// There is nothing special about this table, just a PK, quote text, author and year.
+		$sql0 = 
+			"create table if not exists tbl_quotes (" .
+				"rcdid integer primary key autoincrement not null, " .
+				"quote_text text not null, " .
+				"quote_author text not null, " .
+				"quote_year int null, " .
+				"created_at text not null default current_timestamp);";
+		$this->conn->exec($sql0);
+	}
+
+	function Go ($inConn, $inMethod, $inMaxQuoteLength)
+	{
+		$this->maxQuoteLength = $inMaxQuoteLength;
+		$this->method = $inMethod;
+		//$this->InitIfNeeded (200, array("message" => "Hello from Quotes. Method was [" . $this->method . "]"));
+		//$this->OutJSON();
+		$this->conn = $inConn;
+		$this->CreateTableIfNeeded();
+
+		switch ($this->method)
+		{
+			case "POST" :
+				try
+				{
+					$this->InsertQuote();
+				}
+				catch (Exception $ex)
+				{
+					$messageParts = explode ("|", $ex->getMessage());
+					$this->InitIfNeeded ((int)$messageParts[0], array("message" => "Request failed due to error [" . $messageParts[1] . "]"));
+					$this->OutJSON();
+				}
+				break;
+			default:
+				$this->InitIfNeeded (405, array("message" => "Method [" . $this->method . "] is not supported in Quotes."));
+				$this->OutJSON();
+				break;
+		}
+	}
+
+	function InsertQuote ()
+	{
+		$quoteText = "";
+		if (isset ($_POST['quote_text']))
+			$quoteText = trim($_POST['quote_text']);
+		$quoteAuthor = "";
+		if (isset ($_POST['quote_author']))
+			$quoteAuthor = trim($_POST['quote_author']);
+		$quoteYear = null;
+		if (isset ($_POST['quote_year']))
+		{
+			if (!is_int (trim($_POST['quote_year'])))
+				throw new Exception ("400|If a year is specified it must be a positive integer.");
+			$possibleYear = (int) trim($_POST['quote_year']);
+			if ($possibleYear <= 0)
+				throw new Exception ("400|Year specified wasn't a positive integer.");
+			$quoteYear = $possibleYear;
+		}
+
+		if ("" == $quoteText)
+			throw new Exception ("400|Quote text must be specified.");
+		if (strlen ($quoteText) > $this->maxQuoteLength)
+			throw new Exception ("431|Quote is too long.");
+		if ("" == $quoteAuthor)
+			throw new Exception ("400|Quote author must be specified.");
+
+		$yearInfo = "  No year was specified.";
+		if (!is_null ($quoteYear))
+			$yearInfo = "  Quote year is [" . $quoteYear . "]";
+
+		try
+		{
+			$sql1 = $this->conn->prepare("insert into tbl_quotes (quote_text, quote_author, quote_year) values (:quote_text, :quote_author, :quote_year);");
+			$sql1->execute([":quote_text" => $quoteText, ":quote_author" => $quoteAuthor, ":quote_year" => $quoteYear]);
+
+			$insertID = $this->conn->lastInsertId();
+			$this->InitIfNeeded (200, array ("message" => "Success", "id", $insertID));
+			$this->OutJSON();
+		}
+		catch (Exception $ex)
+		{
+			throw new Exception ("500|Failed due to error [" . $ex->getMessage() . "]");
+		}
+
+	}
+}
+
+?>
---
+++ quotes-examples.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Check for existence of API key file:
+if [ ! -f "./api-key.txt" ]; then
+	echo "api-key.txt doesn't exist or is unreadable."
+	echo "Make sure your API key is in this file."
+	exit 1
+fi
+
+# Copy-Paste your API Key below:
+#API_KEY=Test
+API_KEY=$(cat api-key.txt)
+
+# Make sure the URL matches your application.
+URL=http://my-api-demo:60080/api
+# Applies to quotes only:
+ACTION=Quotes
+
+#printf "GET:\n"
+# Add the -v argument after curl to be verbose.
+#OUTPUT=`curl -d 'key1=val1&key2=val2&key3=val3' --request GET \
+#    --url "$URL/$ACTION" \
+#    --header "Authorization: Bearer $API_KEY"`
+#printf "\nGET Method - Output is:\n$OUTPUT\n\n"
+
+printf "POST:\n"
+OUTPUT=`curl -v -d 'quote_text=Diplomacy%20is%20the%20art%20of%20saying%20%27Nice%20doggie%27%20until%20you%20can%20find%20a%20rock.&quote_author=Will%20Rogers' --request POST \
+    --url "$URL/$ACTION" \
+    --header "authorization: Bearer $API_KEY"`
+printf "\nPOST Method - Output is:\n$OUTPUT\n\n"
+
+#printf "PUT:\n"
+#OUTPUT=`curl -d 'key1=val1&key2=val2&key3=val3' --request PUT \
+#    --url "$URL/$ACTION" \
+#    --header "Authorization: Bearer $API_KEY"`
+#printf "\nPUT Method - Output is:\n$OUTPUT\n\n"
+
+#printf "PATCH:\n"
+#OUTPUT=`curl -d 'key1=val1&key2=val2&key3=val3' --request PATCH \
+#    --url "$URL/$ACTION" \
+#    --header "authorization: Bearer $API_KEY"`
+#printf "\nPATCH Method - Output is:\n$OUTPUT\n\n"
+
+#printf "DELETE:\n"
+#OUTPUT=`curl -d 'key1=val1&key2=val2&key3=val3' --request DELETE \
+#    --url "$URL/$ACTION" \
+#    --header "AuthorizatioN: Bearer $API_KEY"`
+#printf "\nDELETE Method - Output is:\n$OUTPUT\n\n"
+
+
